4f4c0c6b0a0dd05ea33e42f29b89b8bb793c31dd
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2023-07-09 17:52:10 +0530

    Seed Data in Table Villas same commands need to execute in Package Manager Console

--- Maggic_Vila/MaggicVilaAPI/Data/ApplicationDbContext.cs
+++ Maggic_Vila/MaggicVilaAPI/Data/ApplicationDbContext.cs
@@ -9,7 +9,59 @@ namespace MaggicVilaAPI.Data
         { 
         }
         //table name Villas
-        public DbSet<Villa> Villas { get; set; }   
+        public DbSet<Villa> Villas { get; set; }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Villa>().HasData(
+                new Villa
+                {
+                    Id = 1,
+                    Name = "Royal Villa",
+                    Details = "lorem lorem",
+                    ImgUrl = "",
+                    Occupency = 5,
+                    rate = 200,
+                    Sqft = 300,
+                    Amenity = "xyz",
+                    CreatedDate = DateTime.Now
+                },
+                 new Villa
+                 {
+                     Id = 2,
+                     Name = "Royal Villa 1",
+                     Details = "lorem lorem",
+                     ImgUrl = "",
+                     Occupency = 6,
+                     rate = 300,
+                     Sqft = 400,
+                     Amenity = "xyz",
+                     CreatedDate = DateTime.Now
+                 },
+                  new Villa
+                  {
+                      Id = 3,
+                      Name = "Royal Villa 2",
+                      Details = "lorem lorem",
+                      ImgUrl = "",
+                      Occupency = 4,
+                      rate = 100,
+                      Sqft = 150,
+                      Amenity = "xyz",
+                      CreatedDate = DateTime.Now
+                  },
+                   new Villa
+                   {
+                       Id = 4,
+                       Name = "Royal Villa 3",
+                       Details = "lorem lorem",
+                       ImgUrl = "",
+                       Occupency = 7,
+                       rate = 1000,
+                       Sqft = 500,
+                       Amenity = "xyz",
+                       CreatedDate = DateTime.Now
+                   }) ;
+        }
     }
 }
---
+++ Maggic_Vila/MaggicVilaAPI/Migrations/20230709122006_SeedVillaTable.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace MaggicVilaAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedVillaTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Villas",
+                columns: new[] { "Id", "Amenity", "CreatedDate", "Details", "ImgUrl", "Name", "Occupency", "Sqft", "UpdatedDate", "rate" },
+                values: new object[,]
+                {
+                    { 1, "xyz", new DateTime(2023, 7, 9, 17, 50, 6, 505, DateTimeKind.Local).AddTicks(7059), "lorem lorem", "", "Royal Villa", 5, 300, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 200.0 },
+                    { 2, "xyz", new DateTime(2023, 7, 9, 17, 50, 6, 505, DateTimeKind.Local).AddTicks(7077), "lorem lorem", "", "Royal Villa 1", 6, 400, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 300.0 },
+                    { 3, "xyz", new DateTime(2023, 7, 9, 17, 50, 6, 505, DateTimeKind.Local).AddTicks(7080), "lorem lorem", "", "Royal Villa 2", 4, 150, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 100.0 },
+                    { 4, "xyz", new DateTime(2023, 7, 9, 17, 50, 6, 505, DateTimeKind.Local).AddTicks(7082), "lorem lorem", "", "Royal Villa 3", 7, 500, new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified), 1000.0 }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Villas",
+                keyColumn: "Id",
+                keyValue: 4);
+        }
+    }
+}
