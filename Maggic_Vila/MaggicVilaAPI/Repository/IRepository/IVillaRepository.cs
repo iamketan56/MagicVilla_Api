ff56afdf0e14e40c5e2ce961ca2b2e30b61aa3ac
iamketan56 <55191181+iamketan56@users.noreply.github.com>
2023-07-10 18:05:45 +0530

    Villa Respsitory  Implementation

---
+++ Maggic_Vila/MaggicVilaAPI/Repository/IRepository/IVillaRepository.cs
@@ -0,0 +1,16 @@
+﻿using MaggicVilaAPI.Models;
+using System.Linq.Expressions;
+
+namespace MaggicVilaAPI.Repository.IRepository
+{
+    public interface IVillaRepository
+    {
+        Task<List<Villa>> GetAll(Expression<Func<Villa,bool>> filter = null);
+        Task<Villa> Get(Expression<Func<Villa,bool>> filter = null, bool tracked = true);
+        Task Create(Villa entity);
+        Task Remove(Villa entity);
+        Task Save();
+      
+
+    }
+}
---
+++ Maggic_Vila/MaggicVilaAPI/Repository/VillaRepository.cs
@@ -0,0 +1,58 @@
+﻿using AutoMapper;
+using MaggicVilaAPI.Data;
+using MaggicVilaAPI.Models;
+using MaggicVilaAPI.Repository.IRepository;
+using Microsoft.EntityFrameworkCore;
+using System.Linq.Expressions;
+
+namespace MaggicVilaAPI.Repository
+{
+    public class VillaRepository : IVillaRepository
+    {
+        private readonly ApplicationDbContext _Db;
+        public VillaRepository(ApplicationDbContext db)
+        {
+            _Db = db;
+        }
+        public async Task Create(Villa entity)
+        {
+           await _Db.Villas.AddAsync(entity);
+            await Save();
+        }
+
+        public async Task<Villa> Get(Expression<Func<Villa, bool>> filter = null, bool tracked = true)
+        {
+            IQueryable<Villa> query = _Db.Villas;
+            if(!tracked)
+            {
+                query = query.AsNoTracking();
+            }
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.FirstOrDefaultAsync();
+        }
+
+        public async Task<List<Villa>> GetAll(Expression<Func<Villa, bool>> filter = null)
+        {
+            IQueryable<Villa> query = _Db.Villas;
+            if (filter != null)
+            {
+                query = query.Where(filter);
+            }
+            return await query.ToListAsync();
+        }
+
+        public async Task Remove(Villa entity)
+        {
+            _Db.Villas.Remove(entity);
+            await Save();
+        }
+
+        public async Task Save()
+        {
+            await _Db.SaveChangesAsync();
+        }
+    }
+}
